c41e6b70790efcf55604571cf906f2560a67c240
John Oliver Solis <johnoliversolis06@gmail.com>
2026-05-04 21:45:58 +0800

    huh

--- src/App.tsx
+++ src/App.tsx
@@ -677,7 +677,7 @@ const projects: Project[] = [
     color: "text-kurz-orange",
     bg: "bg-kurz-orange",
     icon: Layers,
-    image: "https://images.unsplash.com/photo-1518770660439-4636190af475?q=80&w=1000",
+    image: "/assets/pcb/overview.jpg",
     gallery: ["/assets/pcb/9vfinal.jpg", "/assets/pcb/relayfinal.jpg", "/assets/pcb/locksystempcb.jpg", "/assets/pcb/9vfrontsilkscreen.JPG", "/assets/pcb/relayschematics.jpg", "/assets/pcb/locksystemschematics.jpg"],
     sections: [
       {
